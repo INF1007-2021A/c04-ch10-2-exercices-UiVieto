c3b9f689e92897a6c7849ec02bbd0e66b36eb8c3
Huy Viet Nguyen <90419850+UiVieto@users.noreply.github.com>
2021-11-09 19:54:18 -0500

    V.0.2
    Génération des trains x et y.
    
    "Le Dieu machine est un dieu cruel et assoiffé de sang, réclamant des sacrifices humains pour nourrir son appétit insatiable."

--- exercice.py
+++ exercice.py
@@ -3,21 +3,34 @@
 
 
 import pandas as pd
+import numpy as np
 import sklearn
 
 
 def lire_fichier(fichier: str) -> pd.DataFrame:
-    return pd.read_csv(fichier)
+    return pd.read_csv(fichier, ";")
 
 
-def separer_variables_x_y(fichier: str) -> (pd.DataFrame, pd.DataFrame):
+def separer_variables_x_y(fichier: str) -> (np.array, np.array):
 
-    x = lire_fichier(fichier).drop(columns=["quality"])
-    y = lire_fichier(fichier)["quality"]
-    
+    x = np.array(lire_fichier(fichier).drop(columns=["quality"]))
+    y = np.array(lire_fichier(fichier)["quality"])
+
     return x, y
+
+
+def generer_sous_ensemble_x_y(fichier: str):
+
+    x = separer_variables_x_y(fichier)[0]
+    y = separer_variables_x_y(fichier)[1]
+
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.3, random_state=34)
+
+    return x_train, x_test, y_train, y_test
+
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    pass
+    print(generer_sous_ensemble_x_y("data/winequality-white.csv"))
+    
